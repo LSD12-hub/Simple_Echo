9bfb33375749656c7786f0167a7a44c82100d0d7
ljonas021 <ljonas021@student.wethinkcode.co.za>
2022-03-05 13:41:31 +0200

    added stearms that read and write data

--- src/Client.java
+++ src/Client.java
@@ -1,11 +1,40 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) throws IOException{
     
         System.out.println("Client Started");
+
         Socket socket = new Socket("localhost",1234);
+
+        System.out.println("conneceted");
+
+        // read data
+        BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+
+        // Writes data to socket
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+        // read data from socket
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        
+
+        while (true) {
+            System.out.print("Enter some text: ");
+            String name = userInput.readLine();
+
+            out.println(name);
+            
+            System.out.println(in.readLine());
+
+            if(name.toLowerCase().equals("exit")){
+                break;
+            }
+        }
     }
           
 }
--- src/Server.java
+++ src/Server.java
@@ -1,13 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) throws Exception {
-        System.out.println("Server Started\nWaiting for clients...");
+        System.out.println("Server Started\nWaiting for client...");
 
         ServerSocket serverSocket = new ServerSocket(1234);
         Socket socket = serverSocket.accept();
 
-        System.out.println("conneceted");
+        System.out.println("Connection Established");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        
+
+        while (true) {
+            String text = in.readLine();
+
+            if(text.toLowerCase().equals("exit")) {
+                out.println("thank you for you time");
+                break;
+            }
+
+            out.println("[SERVER] Says " + text);
+        }
     }
 }
